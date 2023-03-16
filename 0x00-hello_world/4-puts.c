b68762db11386ae62c2b205c144d5c2a8d85c83a
oubbaih <eoubbaih@gmail.com>
2023-03-16 04:09:09 -0700

    sd

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main(void){
-
+/**
+ * Description: main function
+ * Return : 0 if no errors
+ */
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
