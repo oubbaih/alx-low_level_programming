bda8b0ead320eb1db542eded51f578cda944c29d
oubbaih <eoubbaih@gmail.com>
2023-03-17 07:49:48 -0700

    sd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Lahcen Oubbaih
+ */
+#inlucde <stdio.h>
+/*
+ *  main - prints the alphabet in lowercase, followed by a new line
+ *
+ *  Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for(letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+
+}
