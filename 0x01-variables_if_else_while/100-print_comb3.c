9b46d98f50598f3243b161f020786221c0be721b
oubbaih <eoubbaih@gmail.com>
2023-03-17 10:29:52 -0700

    sd

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: Lahcen Oubbaih
+ */
+#include <stdio.h>
+/**
+ * main -  prints all possible different combinations of two digits.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{       
+        int dig_1, dig_2;
+        
+        for (dig_1 = 0; dig_2 < 9; dig_1++)
+        {       
+                for (dig_2 = dig_1 + 1; dig_2 < 10; dig_2++)
+                {       
+                        putchar((dig_1 % 10) + '0');
+                        putchar((dig_2 % 10) + '0');
+
+			if (dig_1 == 8 && dig_2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+
+		}
+	}
+
+	putchar('\n');
+	
+	return (0);
+}
+~                                           
