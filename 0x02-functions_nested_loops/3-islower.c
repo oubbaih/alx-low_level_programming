d4a6f221dc715174e382bc98f909a0b61f5b19af
oubbaih <eoubbaih@gmail.com>
2023-03-21 07:13:01 -0700

    sd

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - a function that checks for alphabetic character
+ * _islower - a function that checks for alphabetic character
  * @c: single letter input
  * Return: 1 if c is a letter (lower or uppercase), 0 otherwise
  */
