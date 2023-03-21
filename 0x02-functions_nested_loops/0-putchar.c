56d7947cfb71fb69b53b51898d440eedf9f45f37
oubbaih <eoubbaih@gmail.com>
2023-03-21 05:20:39 -0700

    ds

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
