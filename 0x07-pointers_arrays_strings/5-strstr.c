502ce2b912c27cdde6e7d960332acc13f486be5b
oubbaih <eoubbaih@gmail.com>
2023-04-03 09:08:46 -0700

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,3 +21,23 @@ char *_strstr(char *haystack, char *needle)
 	}
 	return (0);
 }
+/**
+ * coincidence - define if the string b is inside a.
+ * @a: source string
+ * @b: string to be searched
+ *
+ * Return: 1 if there is coincidence, otherwise 0.
+ */
+int coincidence(char *a, char *b)
+{
+	while (*b && *b == *a)
+	{
+		b++;
+		a++;
+	}
+
+	if (*b == '\0')
+		return (1);
+	else
+		return (0);
+}
